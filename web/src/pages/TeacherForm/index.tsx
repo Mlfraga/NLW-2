c2393f772db7c80e0f37951847dbab3edc78c39e
Matheus Fraga <matheuslf44@gmail.com>
2020-08-06 22:21:35 -0300

    first commit

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+function TeachersForm() {
+    return (
+        <PageHeader title="que incrível que você quer dar aulas." />
+    )
+}
+
+export default TeachersForm;
